87939ec50ee77dd71f0486af72ab1dfd2bb36970
Bas Milius <basmilius@hotmail.com>
2026-02-28 08:43:39 +0100

    feat: add getStatus() to MerchantStatisticsService.

--- src/service/MerchantStatisticsService.ts
+++ src/service/MerchantStatisticsService.ts
@@ -3,8 +3,18 @@ import type { ApexOptions } from 'apexcharts';
 
 export class MerchantStatisticsService extends BaseService {
     async getRevenueTrend(merchantId: string): Promise<BaseResponse<ApexOptions>> {
-        return this
+        return await this
             .request(`/merchants/${merchantId}/statistics/revenue-trend`)
+            .method('get')
+            .queryString(QueryString.builder()
+                .append('language', 'nl'))
+            .bearerToken()
+            .run();
+    }
+
+    async getStatus(merchantId: string): Promise<BaseResponse<boolean>> {
+        return await this
+            .request(`/merchants/${merchantId}/statistics/status`)
             .method('get')
             .queryString(QueryString.builder()
                 .append('language', 'nl'))
